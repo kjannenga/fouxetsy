3c9892a71cd868291dcf5c786911ba781a8ae854
KrystineJannenga <kjannenga1@gmail.com>
2017-04-04 13:00:31 -0700

    lol jk now the images work

--- main.js
+++ main.js
@@ -6,7 +6,14 @@ $(document).ready(function(){
    		formData.results.forEach(function (sale){
    			
 
-	   			results += `<div class="sale"> <a href="${sale.url}"> <img src="${sale.Images.url_570xN}"/><div class="info">${sale.title}<br />${sale.Shop.shop_name} ${sale.price}</a></div></div>`
+	   			results += `<div class="sale">
+	   						 <a href="${sale.url}"> <img src="${sale.Images[0].url_570xN}"/>
+	   						 	<div class="info">
+	   						 		${sale.title}<br />
+	   						 		${sale.Shop.shop_name} ${sale.price}
+	   						 	</div>
+	   						 </a>
+	   						 </div>`
 
    		})
    		$('main').html(results)
